ecf9fe5215e3bd1c46407eaba24543de5571d40d
root <mem375@pitt.edu>
2020-06-17 15:47:53 -0400

    ISLE_DEV initial setup

--- scripts/proxy/ssl-certs/local.sh
+++ scripts/proxy/ssl-certs/local.sh
@@ -5,5 +5,5 @@
 # Add the name of your local site name to CN=.localdomain (e.g. CN=yourprojectnamehere.localdomain)
 # Optional: You may change the organization ("O=") name (e.g. O=Your Organization)
 
-openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout ../../../config/proxy/ssl-certs/.localdomain.key -out ../../../config/proxy/ssl-certs/.localdomain.pem \
-	-subj "/C=US/O=Islandora Collaboration Group/OU=Domain Control Validated/CN=.localdomain"
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout ../../../config/proxy/ssl-certs/ISLE_DEV.localdomain.key -out ../../../config/proxy/ssl-certs/ISLE_DEV.localdomain.pem \
+	-subj "/C=US/O=Islandora Collaboration Group/OU=Domain Control Validated/CN=ISLE_DEV.localdomain"
