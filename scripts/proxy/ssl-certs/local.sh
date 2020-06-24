000134cd4891cf7a2a722639f2945f6df44c699b
root <mem375@pitt.edu>
2020-06-24 13:41:18 -0400

    redo with localdomain name change

--- scripts/proxy/ssl-certs/local.sh
+++ scripts/proxy/ssl-certs/local.sh
@@ -5,5 +5,5 @@
 # Add the name of your local site name to CN=.localdomain (e.g. CN=yourprojectnamehere.localdomain)
 # Optional: You may change the organization ("O=") name (e.g. O=Your Organization)
 
-openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout ../../../config/proxy/ssl-certs/ISLE_DEV.localdomain.key -out ../../../config/proxy/ssl-certs/ISLE_DEV.localdomain.pem \
-	-subj "/C=US/O=Islandora Collaboration Group/OU=Domain Control Validated/CN=ISLE_DEV.localdomain"
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout ../../../config/proxy/ssl-certs/ISLEDEV.localdomain.key -out ../../../config/proxy/ssl-certs/ISLEDEV.localdomain.pem \
+	-subj "/C=US/O=Islandora Collaboration Group/OU=Domain Control Validated/CN=ISLEDEV.localdomain"
